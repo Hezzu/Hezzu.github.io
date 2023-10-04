2ed25bdc64f0ddab1e25b368697ec98a2be0dec6
Hezzu <M3guM3gu@gmail.com>
2023-10-04 23:08:10 +0200

    Kinda adding anything to the website

---
+++ Javascript/clickers.js
@@ -0,0 +1,67 @@
+let clockType = false;
+function updateTime() {
+
+  
+  let dateInfo = new Date();
+  let hr, _min;
+  let clock = document.getElementById("clock");
+  hr = dateInfo.getHours(),
+  _min = (dateInfo.getMinutes() < 10) ? "0" + dateInfo.getMinutes() : dateInfo.getMinutes();
+
+  if(clockType)
+    {
+        if (dateInfo.getHours() == 0) {
+          hr = 12;
+        } else if (dateInfo.getHours() > 12) {
+          hr = dateInfo.getHours() - 12;
+        } else {
+          hr = dateInfo.getHours();
+        }
+        let am = document.getElementsByClassName("am")[0];
+        let pm = document.getElementsByClassName("pm")[0];
+        if(dateInfo.getHours() > 12)
+        {
+          am.innerHTML = "AM";
+          pm.innerHTML = "PM";
+          pm.style.color = getComputedStyle(document.documentElement).getPropertyValue("--TextColor");
+          am.style.opacity = "0.5";
+        }else{
+          am.innerHTML = "AM";
+          pm.innerHTML = "PM";
+          am.style.color = getComputedStyle(document.documentElement).getPropertyValue("--TextColor");
+          pm.style.opacity = "0.5";
+        }
+      } else{
+        document.getElementsByClassName("am")[0].innerHTML = "";
+        document.getElementsByClassName("pm")[0].innerHTML = "";
+      }
+  
+    /* time */
+
+    let currentTime = hr + ":" + _min  + " ";
+  
+    // print time
+    document.getElementsByClassName("hms")[0].innerHTML = currentTime;
+  };
+function changeTimeType(){
+  clockType = !clockType;
+  clock.style.opacity = 0;
+  setTimeout(()=> {
+    updateTime();
+    clock.style.opacity = 1;
+  }, 100);
+}
+updateTime(clockType);
+setInterval(() => {
+  updateTime()
+}, 1000);
+
+
+// End of Clock Script | Start of Name Change
+var nickVar = false;
+function nameChange(){
+  nickVar = !nickVar;
+  let nick = document.getElementById("nick");
+
+  nickVar == false ? nick.innerHTML = "🌸Megu Megu🌸" : nick.innerHTML = "🌸Hezzu🌸";
+}
